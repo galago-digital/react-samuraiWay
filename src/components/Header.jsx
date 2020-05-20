3b04ee307a6a9a5a9c7dc19eee1d345c94fc4e1c
NazarovDs <merl4210@gmail.com>
2020-05-21 00:11:46 +0500

    samuraiway

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () =>{
+return (
+<header className="header">
+        <img src="https://img.icons8.com/ios-filled/2x/dot-logo.png"></img>
+</header>
+)}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img className = "lending" src="https://jssors8.azureedge.net/demos/image-slider/img/faded-monaco-scenery-evening-dark-picjumbo-com-image.jpg" />
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                my posts
+                <div>
+                            new post
+                </div>
+                        <div>
+                            posts
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Profile
